c836f7f867f7eae4fa1b944e209d3d20c22a26c0
shortpathfirst <andrea.zurli94@gmail.com>
2024-05-18 21:30:46 +0200

    Grid

--- src/Visualizer/GridPalline.tsx
+++ src/Visualizer/GridPalline.tsx
@@ -10,87 +10,86 @@ import { Node } from '../model/Grid';
 import { Dijkstra } from '../Algorithm/Dijkstra';
 
 let operations:Operation[] = [];
-
+const enum grid{
+    draw=-1,
+    start=0,
+    finish=1,
+}
 function GridPalline() {
-    // const initMatrix = () => new Grid().matrix; //To not call constructor everytime it render
-    // const [matrix,setMatrix] = useState(initMatrix);
 
-    const nodes = ()=>new Grid().nodes;
-    const [matrix,setMatrix] = useState(nodes)
-    const [isStart,changeStart] = useState(-1);
-    const [go,setGo] = useState(false);
-    const [color, setColor] = useColor("#561ecb"); //Palette
-    const [colorStory,setColorStory] = useState<IColor[]>([]);
-    const [eraser,setEraser] = useState(false);
+    const nodes = ()=>new Grid().nodes;                         //To not call constructor everytime it render
+    const [matrix,setMatrix] = useState(nodes);                 //Grid matrix
+    const [isStart,changeStart] = useState(grid.draw);          //Maze start & finish
+    const [draw,setDraw] = useState(false);                     //Activate pen
+    const [color, setColor] = useColor("#561ecb");              //Palette
+    const [colorStory,setColorStory] = useState<IColor[]>([]);  //List of color used
+    const [eraser,setEraser] = useState(false);                 //Eraser
 
     useEffect(() => {
-        window.addEventListener('mouseup', ()=>setGo(false), false); 
+        window.addEventListener('mouseup', ()=>setDraw(false), false); 
     },[])//add listener 1 time only
 
     function handleHover(i:number,j:number){
-        if(go){
+        if(draw){
             let copy = [...matrix];
-            operations.push({i:i,j:j,color:eraser?'':color.hex,prevColor:copy[i][j].value});
+            operations.push({
+                i:i,
+                j:j,
+                color:eraser?'':color.hex,
+                prevColor:copy[i][j].value});
             copy[i][j].value=eraser?'':color.hex;
-            copy[i][j].isWall=eraser?false:true;
+            copy[i][j].isWall=copy[i][j].value===color.hex || !eraser?true:false;//if color i paint different than prev
             setMatrix(copy);
         }
-      
     }
 
     function handleRight(e:any,i:number,j:number){
         e.preventDefault();
-        setGo(false);
+        setDraw(false);
         let copy = [...matrix]
-        operations.push({i:i,j:j,color:color.hex,prevColor:copy[i][j].value});
-        copy[i][j].isWall=copy[i][j].value!==color.hex?true:false; //if color i paint different than prev 
+        operations.push({
+            i:i,
+            j:j,
+            color:color.hex,
+            prevColor:copy[i][j].value});
+            copy[i][j].isWall=copy[i][j].value===color.hex || !eraser?true:false; //if color i paint different than prev 
         copy[i][j].value='';
         setMatrix(copy);
     }
     function handleClick(i:number,j:number){
         let copy = [...matrix];
-        if(isStart===0){
+        if(isStart === grid.start){
             copy[i][j].value='#01ff00';
             Grid.START_NODE_ROW = i;
             Grid.START_NODE_COL = j;
+            changeStart(grid.finish);
             setMatrix(copy);
-            changeStart(1);
             return;
-        }else if(isStart===1){
-
+        }
+        if(isStart === grid.finish){
             copy[i][j].value='#fe0000';
             Grid.FINISH_NODE_ROW = i;
             Grid.FINISH_NODE_COL = j;
-            setMatrix(copy);
-            changeStart(-1);
-            return;
-        }
-        setGo(true);
-        if(eraser){
-            operations.push({i:i,j:j,color:'',prevColor:copy[i][j].value});
-            copy[i][j].value='';
-            copy[i][j].isWall=false;
+            changeStart(grid.draw);
             setMatrix(copy);
             return;
         }
-        pushColor(color);
+        setDraw(true);
+        if(!eraser) pushColor(color);
         operations.push({i:i,j:j,color:eraser?'':color.hex,prevColor:copy[i][j].value});
         copy[i][j].isWall=eraser?false:true;
         copy[i][j].value = eraser?'':color.hex;
-        
         setMatrix(copy);
     }
     const pushColor = (color:IColor) =>{
-
         if(colorStory.indexOf(color) === -1) {
             setColorStory([...colorStory,color]);
         }
     }
 
-   function handlePrevState(){
+    function handlePrevState(){
     let count=0;
     let copy = [...matrix];
-    // let operationCopy = [...operation];
     while(operations.length>0 && (count <10)){
         let lastoperation:Operation = operations.pop()!;
         let i = lastoperation.i;
@@ -98,59 +97,57 @@ function GridPalline() {
         copy[i][j].value=lastoperation.prevColor;
         count++;
     }
-    // setOperation(operationCopy);
     setMatrix(copy);
-   }
-   function handleClear(){
-    setMatrix(new Grid().nodes);
-    // setOperation([]);
-    operations=[];
-   }
-function animateDijkstra(visitedNodes:Node[],nodesInshortestPath:Node[]){
-    for (let i = 0; i < visitedNodes.length; i++) {
-        if (i === visitedNodes.length-1) {
-          setTimeout(() => {
-            animateShortestPath(nodesInshortestPath);
-          }, 10 * i);
-          return;
-        }
-        setTimeout(() => {
-          const node = visitedNodes[i];
-          let copy = [...matrix];
-          copy[node.row][node.col].value="#aee4ac";
-          setMatrix(copy);
-        }, 10 * i);
-      }
-
-}
-function animateShortestPath(nodesInshortestPath:Node[]) {
-    for (let i = 0; i < nodesInshortestPath.length; i++) {
-      setTimeout(() => {
-        const node = nodesInshortestPath[i];
-        let copy = [...matrix];
-        copy[node.row][node.col].value='#cb4d1e';
-        setMatrix(copy);
-      }, 50 * i);
     }
-  }
-function visualizeDijkstra(){
-    const grid =matrix;
-    const startNode = grid[Grid.START_NODE_ROW][Grid.START_NODE_COL];
-    const finishNode = grid[Grid.FINISH_NODE_ROW][Grid.FINISH_NODE_COL];
-    const algorithm = new Dijkstra();
-    const visitedNodes = algorithm.dijkstra(grid,startNode,finishNode);
-    const nodesInshortestPath = algorithm.getNodesInShortestPathOrder(finishNode);
-    animateDijkstra(visitedNodes!,nodesInshortestPath); //DO NOT FORCE !
-    
-}   
-function addLine(){
-    let width=matrix[0].length;
-    let heigth=matrix.length+1;
-    const init = new Grid(width,heigth);
-    setMatrix(init.nodes);
+    function handleClear(){
+        setMatrix(new Grid().nodes);
+        operations=[];
+    }
+    function animateDijkstra(visitedNodes:Node[],nodesInshortestPath:Node[]){
+        for (let i = 0; i < visitedNodes.length; i++) {
+            if (i === visitedNodes.length-1) { //Wait animation
+            setTimeout(() => {
+                animateShortestPath(nodesInshortestPath);
+            }, 10 * i);
+            return;
+            }
+            setTimeout(() => {
+            const node = visitedNodes[i];
+            let copy = [...matrix];
+            copy[node.row][node.col].value="#aee4ac";
+            setMatrix(copy);
+            }, 10 * i);
+        }
 
-}
-function removeLine(){
+    }
+    function animateShortestPath(nodesInshortestPath:Node[]) {
+        for (let i = 0; i < nodesInshortestPath.length; i++) {
+        setTimeout(() => {
+            const node = nodesInshortestPath[i];
+            let copy = [...matrix];
+            copy[node.row][node.col].value='#cb4d1e';
+            setMatrix(copy);
+        }, 50 * i);
+        }
+    }
+    function visualizeDijkstra(){
+        const grid =matrix;
+        const startNode = grid[Grid.START_NODE_ROW][Grid.START_NODE_COL];
+        const finishNode = grid[Grid.FINISH_NODE_ROW][Grid.FINISH_NODE_COL];
+        const algorithm = new Dijkstra();
+        const visitedNodes = algorithm.dijkstra(grid,startNode,finishNode);
+        const nodesInshortestPath = algorithm.getNodesInShortestPathOrder(finishNode);
+        animateDijkstra(visitedNodes!,nodesInshortestPath); //DO NOT FORCE !
+        
+    }   
+    function addLine(){
+        let width=matrix[0].length;
+        let heigth=matrix.length+1;
+        const init = new Grid(width,heigth);
+        setMatrix(init.nodes);
+
+    }
+    function removeLine(){
     let width=matrix[0].length;
     let heigth=matrix.length-1;
     const init = new Grid(width,heigth);
@@ -159,11 +156,11 @@ function removeLine(){
 
   return (
     <>
-    <div className='container' key={'c'}>
+    <div className='container' >
         <div className='prevColorBox'>
         {
             colorStory.map((el)=>{
-                return <div className='prevColor'><button className='colorBox' 
+                return <div key={`div ${el.hex}`} className='prevColor'><button className='colorBox' 
                 key={`color ${el.hex}`}
                 onClick={()=>setColor(el)} 
                 onContextMenu={(e)=>{
@@ -171,7 +168,7 @@ function removeLine(){
                     setColorStory(colorStory.filter(item => item !== el));
                 }}
                 style={{backgroundColor:el.hex}}>
-                </button>{el.hex}</div>
+                </button >{el.hex}</div>
             })
         }
         </div>
@@ -189,7 +186,7 @@ function removeLine(){
                                 onContextMenu={(e: any)=>{handleRight(e,i,j)}} 
                                 onMouseEnter={()=>handleHover(i,j)} 
                                 onPointerDown={()=>handleClick(i,j)} 
-                                onPointerUp={()=>setGo(false)}
+                                onPointerUp={()=>setDraw(false)}
                                 color={(matrix[i][j].value==='')?'':matrix[i][j].value}
                                 />
                             )
@@ -210,7 +207,7 @@ function removeLine(){
             <button className='backButton' onClick={() =>visualizeDijkstra()}>
             Maze game
             </button>
-            <button className='backButton' onClick={() => changeStart(0)}>
+            <button className='backButton' onClick={() => changeStart(grid.start)}>
              Set Start and finish
             </button>
             <button className='backButton' onClick={() => addLine()}>
